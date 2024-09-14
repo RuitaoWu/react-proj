d5c4f187b3683dcefe65cbbd9fe9e4af50ee6cc8
RuitaoWu <ruitaowu0@gmail.com>
2024-09-14 11:11:28 -0700

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
 import BannerCategories from "./ui/BannerCategories"
 import "react-multi-carousel/lib/styles.css";
+import HomeBanner from "./ui/Homebanner";
+import Hightlights from "./ui/Hightlights";
+import Categories from "./ui/Categories";
+import ProductList from "./ui/ProductList";
 
 function App() {
   return (
     <main>
       <main>
         <BannerCategories />
+        <HomeBanner />
+        <Hightlights />
+        <Categories />
+        <ProductList />
       </main>
 
     </main>
--- client/src/ui/ProductCard.tsx
+++ client/src/ui/ProductCard.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 import { MdOutlineStarOutline } from "react-icons/md";
 import { ProductProps } from "../../type";
-// import AddToCartBtn from "./AddToCartBtn";
+import AddToCartBtn from "./AddToCartBtn";
 import { useState } from "react";
 import {
   Button,
@@ -11,11 +13,10 @@ import {
   TransitionChild,
 } from "@headlessui/react";
 import FormattedPrice from "./FormattedPrice";
-
+import ProductCardSideNav from "./ProductCardSideNav";
 import { useNavigate } from "react-router-dom";
 interface Props {
   item: ProductProps;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   setSearchText?: any;
 }
 
@@ -34,8 +35,7 @@ const ProductCard = ({ item, setSearchText }: Props) => {
 
   const handleProduct = () => {
     navigation(`/product/${item?._id}`);
-    // setSearchText && setSearchText("");
-    setSearchText("");
+    setSearchText && setSearchText("");
   };
   return (
     <div className="border border-gray-200 rounded-lg p-1 overflow-hidden hover:border-black duration-200 cursor-pointer">
@@ -52,7 +52,7 @@ const ProductCard = ({ item, setSearchText }: Props) => {
           alt="productImage"
           className="w-full h-full rounded-md object-cover group-hover:scale-110 duration-300"
         />
-        {/* <ProductCardSideNav product={item} /> */}
+        <ProductCardSideNav product={item} />
       </div>
       <div className="flex flex-col gap-2 px-2 pb-2">
         <h3 className="text-xs uppercase font-semibold text-lightText">
@@ -66,7 +66,7 @@ const ProductCard = ({ item, setSearchText }: Props) => {
           <MdOutlineStarOutline />
           <MdOutlineStarOutline />
         </div>
-        {/* <AddToCartBtn product={item} /> */}
+        <AddToCartBtn product={item} />
       </div>
       <Transition appear show={isOpen}>
         <Dialog
